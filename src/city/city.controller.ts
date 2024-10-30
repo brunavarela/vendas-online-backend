a0ec748df01f84e34adba83283a324797c0e8e06
Bruna Varela <brunavarela1@hotmail.com>
2024-10-30 20:40:08 -0300

    feat: create controller and service cities

---
+++ src/city/city.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { CityService } from './city.service';
+import { CityEntity } from './entities/city.entity';
+
+@Controller('city')
+export class CityController {
+  constructor(
+    private readonly cityService: CityService,
+  ) {}
+  
+  @Get('/:stateId')
+  async getAllCitiesByStateId(): Promise<CityEntity[]> {
+    return this.cityService.getAllCitiesByStateId(4);
+  }
+}
+
