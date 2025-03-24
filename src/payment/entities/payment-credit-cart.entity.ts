236f60790b9479f41c234b8195f6f11e5420d1da
Bruna Varela <brunavarela1@hotmail.com>
2025-03-24 14:04:29 -0300

    feat: create paymentEntity

---
+++ src/payment/entities/payment-credit-cart.entity.ts
@@ -0,0 +1,9 @@
+
+import { ChildEntity, Column } from "typeorm";
+import { PaymentEntity } from "./payment.entity";
+
+@ChildEntity()
+export class PaymentCreditCartEntity extends PaymentEntity {
+  @Column({ name: 'amount_payments', nullable: false })
+  amountPayments: number;
+}
