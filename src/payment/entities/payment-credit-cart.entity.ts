6509549304a92f0320afb82fced781362c8eb783
Bruna Varela <brunavarela1@hotmail.com>
2025-03-25 18:36:54 -0300

    feat: create paymentService

--- src/payment/entities/payment-credit-cart.entity.ts
+++ src/payment/entities/payment-credit-cart.entity.ts
@@ -1,9 +1,20 @@
-
 import { ChildEntity, Column } from "typeorm";
 import { PaymentEntity } from "./payment.entity";
+import { CreateOrderDTO } from "src/order/dtos/createOrder.dto";
 
 @ChildEntity()
-export class PaymentCreditCartEntity extends PaymentEntity {
+export class PaymentCreditCardEntity extends PaymentEntity {
   @Column({ name: 'amount_payments', nullable: false })
   amountPayments: number;
+
+  constructor(
+    statusId: number, 
+    price: number, 
+    discount: number, 
+    finalPrice: number,
+    createOrderDTO: CreateOrderDTO
+  ) {
+      super(statusId, price, discount, finalPrice);
+      this.amountPayments = createOrderDTO?.amountPayments || 0;
+    }
 }
--- src/payment/entities/payment-pix.ts
+++ src/payment/entities/payment-pix.ts
@@ -1,12 +1,28 @@
 
 import { ChildEntity, Column } from "typeorm";
 import { PaymentEntity } from "./payment.entity";
+import { CreateOrderDTO } from "src/order/dtos/createOrder.dto";
 
 @ChildEntity()
 export class PaymentPixEntity extends PaymentEntity {
   @Column({ name: 'code', nullable: false })
-  code: number;
+  code: string;
 
   @Column({ name: 'date_payment', nullable: false })
   datePayment: Date;
+
+  // quando tem herança, estamos herdando as infos do pai
+  // entao, precisa dar um super (construtor do pai),
+  // pois precisa construir os dados do pai e depois do filho
+  constructor(
+    statusId: number, 
+    price: number, 
+    discount: number, 
+    finalPrice: number,
+    createOrderDTO: CreateOrderDTO
+  ) {
+      super(statusId, price, discount, finalPrice);
+      this.code = createOrderDTO.codePix || '';
+      this.datePayment = new Date(createOrderDTO.datePayment || '')
+    }
 }
