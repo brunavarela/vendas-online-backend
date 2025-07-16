1ec36f5e979d6a76991e4b7ce7994dedc8735549
Bruna Varela <brunavarela1@hotmail.com>
2025-07-16 12:10:59 -0300

    fix: migrations

--- src/migration/1742313029709-alter-table-cart.ts
+++ src/migration/1742313029709-alter-table-cart.ts
@@ -2,9 +2,9 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class alterTableCart1675939002410 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.query(`
-            ALTER TABLE cart ADD active boolean NOT NULL;
-        `);
+    // queryRunner.query(`
+    //         ALTER TABLE cart ADD active boolean NOT NULL;
+    //     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
